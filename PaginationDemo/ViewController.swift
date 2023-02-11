915d9e8773927b4cb0cc20102f8798e0611f91d2
Raman Singh <rsingh32@gmail.com>
2023-02-10 17:30:33 -0700

    Initial Commit

---
+++ PaginationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaginationDemo
+//
+//  Created by Raman Singh on 2023-02-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
